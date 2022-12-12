bbbe01b34c2b48c312eda017970f1e588987bcc3
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-12 11:45:32 +0900

    計算機能の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
+  const priceInput = document.getElementById('item-price');
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value.toLocaleString();
+    const tax = 0.1;
+    const taxFee = Math.floor(inputValue * tax);
 
+    const addTaxDom = document.getElementById('add-tax-price');
+    addTaxDom.innerHTML = taxFee.toLocaleString();
+
+    const profit = document.getElementById('profit');
+    profit.innerHTML = Math.floor(inputValue - taxFee).toLocaleString();
+  })
 });
