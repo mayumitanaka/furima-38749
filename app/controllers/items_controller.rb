bc8f62aa248c6845594a64e89e593d91d235aa87
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-12 19:58:56 +0900

    Rubocopの修正反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,10 +36,10 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
-      redirect_to root_path
-    end
+    return unless item.user_id == current_user.id
+
+    item.destroy
+    redirect_to root_path
   end
 
   private
