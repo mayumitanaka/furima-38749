65b47ecfe63785bd330d5788a3612efb05ea6c4c
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2023-01-10 01:02:40 +0900

    デフォルトブランチ名の設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'furima-38749'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:mayumitanaka/furima-38749.git'
 
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
